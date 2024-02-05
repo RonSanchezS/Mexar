411dd98637027af7a8a253f40181bd6d09566e88
Gary Sanchez <ronald.sanchez@jala.university>
2024-02-05 18:25:50 -0400

    Form page, no function yet

---
+++ cotizar/index.js
@@ -0,0 +1,33 @@
+let intersectingFlag = false;
+
+const navbar = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            document.querySelector(".headernav").classList.remove("move-up", "move-down", "is-intersecting");
+            intersectingFlag = true;
+        } else {
+            document.querySelector(".headernav").classList.add("move-up");
+            intersectingFlag = false;
+        }
+    });
+});
+const navbarElement = document.querySelector(".uber_text");
+navbar.observe(navbarElement);
+
+var lastScrollTop = 0;
+
+window.onscroll = function() {
+    var currentScrollTop = window.scrollY || document.documentElement.scrollTop;
+
+    if (currentScrollTop > lastScrollTop) {
+
+        if(!intersectingFlag) {
+        //    document.querySelector(".headernav").classList.add("move-up");
+        }
+       
+    } else {
+       // document.querySelector(".headernav").classList.remove("move-up", "move-down");
+    }
+
+    lastScrollTop = currentScrollTop <= 0 ? 0 : currentScrollTop;
+};
